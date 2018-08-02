fb531273cf3e17913c929dbde2c3a98cb7230d0e
Ramon Ribeiro <rhpr@cesar.org.br>
2018-08-02 15:18:15 -0300

    knot: Add header for states in fsm
    Add states available in KNoT finite state machine.
    
    Signed-off-by: Ramon Ribeiro <rhpr@cesar.org.br>

---
+++ subsys/knot/include/states.h
@@ -0,0 +1,21 @@
+/**
+ * Put your licence here
+ */
+
+#ifndef __KNOT_STATE_H
+#define __KNOT_STATE_H
+/* KNoT protocol client states */
+enum knot_state {
+	STATE_DISCONNECTED=0,
+	STATE_ACCEPTING,
+	STATE_CONNECTED,
+	STATE_AUTHENTICATING,
+	STATE_REGISTERING,
+	STATE_SCHEMA,
+	STATE_SCHEMA_RESP,
+	STATE_ONLINE,
+	STATE_RUNNING,
+	STATE_ERROR
+};
+
+#endif // __KNOT_STATE_H
